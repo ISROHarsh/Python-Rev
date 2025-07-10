f55b9070223603e463850d4aa0390ae783a9de98
ISROHarsh <gamoaster@gmail.com>
2025-07-10 08:55:49 +0530

    Completed Set 1

---
+++ Set 1.py
@@ -0,0 +1,18 @@
+#1. Write a program to print Twinkle twinkle little star poem in python.
+print("""Twinkle Twinkle little star
+      how i wonder what you are
+      up above the world so hight
+      like a diamond in the sky""")
+
+#2. Use REPL and print the table of 5 using it.
+'''DONE, opened the terminal and gave the command as per requriment.'''
+
+#3. Install an external module and use it to perform an operation of your interest.
+'''DONE, Installed the pandas and used a random command.'''
+
+#4. Write a python program to print the contents of a directory using the os module. Search online for the function which does that.
+import os  # Imported the os module
+print(os.listdir()) # printing the contents of a directory by using the listdir() cmd in os module.
+
+#5. Label the program written in problem 4 with comments.  
+'''DONE'''
